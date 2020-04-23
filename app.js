b6627ef3b7329a1a7364d5a97485153a545e0e40
Tommy So <sofuming@gmail.com>
2020-04-23 12:08:28 +0800

    Install GraphQL, build schema

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const usersRoute = require('./routes/user.route');
 const iconRoute = require('./routes/icon.route');
 const express = require('express');
 const app = express();
-
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql')
 
 if(!config.get('myprivatekey')) {
     console.log('Private key is not defined.')
@@ -24,7 +25,32 @@ connection.once('open', () => {console.log('Connected to MongoDB.')})
 app.use(express.json());
 app.use('/api/users', usersRoute)
 app.use('/api/icon', iconRoute)
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
 
+        type RootMutation {
+            createEvent (name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Hello', 'World!']
+        },
+        createEvent: (args) => {
+           const eventName = args.name
+           return eventName 
+        }
+    },
+    graphiql: true
+}))
 
 // const port = process.env.PORT || 3000;
 app.listen(3000, () => console.log('Listening to port 3000.'))
